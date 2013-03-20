f6043274bf4f8d7bf5872950d4e968242e89eee3
Eric Shull <shull@atomicobject.com>
2013-03-19 20:56:03 -0400

    Port of Python script.

---
+++ levenshtein.go
@@ -0,0 +1,51 @@
+package levenshtein
+
+func count(start, stop int) []int {
+	var items []int
+	for i := start; i < stop; i++ {
+		items = append(items, i)
+	}
+	return items
+}
+
+func pad(head, tailLength int) []int {
+	items := []int{head}
+	for i := 0; i < tailLength; i++ {
+		items = append(items, 0)
+	}
+	return items
+}
+
+func min(a ...int) int {
+	m := int(^uint(0) >> 1) // largest int
+	for _, i := range a {
+		if i < m {
+			m = i
+		}
+	}
+	return m
+}
+
+func LevenshteinDistance(a, b string) int {
+	n, m := len(a), len(b)
+	// Make sure n <= m to use O(min(n,m)) space
+	if n > m {
+		a, b = b, a
+		n, m = m, n
+	}
+
+	current := count(0, n + 1)
+	var previous []int
+	for i := 1; i <= m; i++ {
+		previous, current = current, pad(i, n)
+		for j := 1; j <= n; j++ {
+			add, del := previous[j] + 1, current[j - 1] + 1
+			change := previous[j - 1]
+			if a[j - 1] != b[i - 1] {
+				change++
+			}
+			current[j] = min(add, del, change)
+		}
+	}
+	return current[n]
+}
