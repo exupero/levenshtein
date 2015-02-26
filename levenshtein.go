a43a913ca96eb5d6494a3a5a9025d6d00c760b92
Eric Shull <shull@atomicobject.com>
2015-02-26 06:33:02 -0500

    Add function for getting distance by whole word edits instead of letter edits.

--- levenshtein.go
+++ levenshtein.go
@@ -1,5 +1,9 @@
 package levenshtein
 
+import (
+	"strings"
+)
+
 func count(start, stop int) []int {
 	var items []int
 	for i := start; i < stop; i++ {
@@ -42,6 +46,31 @@ func LevenshteinDistance(a, b string) int {
 			add, del := previous[j] + 1, current[j - 1] + 1
 			change := previous[j - 1]
 			if a[j - 1] != b[i - 1] {
+				change++
+			}
+			current[j] = min(add, del, change)
+		}
+	}
+	return current[n]
+}
+
+func LevenshteinWordDistance(a, b string) int {
+	c, d := strings.Split(a, " "), strings.Split(b, " ")
+	n, m := len(c), len(d)
+	// Make sure n <= m to use O(min(n,m)) space
+	if n > m {
+		c, d = d, c
+		n, m = m, n
+	}
+
+	current := count(0, n + 1)
+	var previous []int
+	for i := 1; i <= m; i++ {
+		previous, current = current, pad(i, n)
+		for j := 1; j <= n; j++ {
+			add, del := previous[j] + 1, current[j - 1] + 1
+			change := previous[j - 1]
+			if c[j - 1] != d[i - 1] {
 				change++
 			}
 			current[j] = min(add, del, change)
